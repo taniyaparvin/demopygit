123b362ee078036bc09fc50b7f2ea7624142e4b4
Tani <taniyaele@gmail.com>
2026-05-02 20:05:06 +0530

    changes in demo file added some lines

--- demo.py
+++ demo.py
@@ -2,3 +2,7 @@ print("hello!")
 
 print("heyy ...")
 print("testing...")
+print("using git and github..")
+
+# working on git command how to download any project from git 
+# how to generate ssh key
